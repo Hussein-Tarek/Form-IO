5bda4f80fa437df9d09fec4366eb6e1bf711ea37
Hussein Tarek <htarek47@gmail.com>
2024-04-24 13:36:33 +0200

    update components

--- customInputs.js
+++ customInputs.js
@@ -1,81 +1,11 @@
 (function () {
   "use strict";
 
-  var customComponents = {
-    Age: {
-      title: "Age",
-      key: "age",
-      icon: "check",
-      schema: {
-        label: "Age",
-        applyMaskOn: "change",
-        mask: false,
-        tableView: true,
-        delimiter: false,
-        requireDecimal: false,
-        inputFormat: "plain",
-        truncateMultipleSpaces: false,
-        key: "age",
-        conditional: {
-          show: true,
-          conjunction: "all",
-          conditions: [
-            {
-              component: "name",
-              operator: "isNotEmpty",
-            },
-          ],
-        },
-        type: "number",
-        input: true,
-      },
-    },
-    Accept: {
-      title: "Accept",
-      key: "accept",
-      icon: "check",
-      schema: {
-        label: "Accept Income",
-        optionsLabelPosition: "right",
-        inline: true,
-        tableView: false,
-        defaultValue: "accept",
-        values: [
-          {
-            label: "Accept",
-            value: "accept",
-            shortcut: "",
-          },
-          {
-            label: "Refuse",
-            value: "refuse",
-            shortcut: "",
-          },
-        ],
-        key: "acceptIncome",
-        conditional: {
-          show: true,
-          conjunction: "all",
-          conditions: [
-            {
-              component: "calculatedIncome",
-              operator: "isNotEmpty",
-            },
-          ],
-        },
-        type: "radio",
-        input: true,
-      },
-    },
-  };
-
   window.Formio.use({
-    framework: "yourFramework",
-    components: customComponents,
     options: {
       builder: {
         builder: {
-          custom: {
+          x: {
             title: "Fields Pool",
             weight: 10,
             components: {
@@ -200,123 +130,6 @@
                   ],
                 },
               },
-              column: {
-                title: "Address",
-                schema: {
-                  label: "Address",
-                  columns: [
-                    {
-                      components: [
-                        {
-                          label: "Country",
-                          applyMaskOn: "change",
-                          tableView: true,
-                          key: "country",
-                          type: "textfield",
-                          input: true,
-                        },
-                      ],
-                      width: 4,
-                      offset: 0,
-                      push: 0,
-                      pull: 0,
-                      size: "md",
-                      currentWidth: 4,
-                    },
-                    {
-                      components: [
-                        {
-                          label: "City",
-                          applyMaskOn: "change",
-                          tableView: true,
-                          key: "city",
-                          type: "textfield",
-                          input: true,
-                        },
-                      ],
-                      width: 4,
-                      offset: 0,
-                      push: 0,
-                      pull: 0,
-                      size: "md",
-                      currentWidth: 4,
-                    },
-                    {
-                      components: [
-                        {
-                          label: "Street",
-                          applyMaskOn: "change",
-                          tableView: true,
-                          key: "street",
-                          type: "textfield",
-                          input: true,
-                        },
-                      ],
-                      size: "md",
-                      width: 4,
-                      currentWidth: 4,
-                    },
-                  ],
-                  key: "address",
-                  type: "columns",
-                  input: false,
-                  tableView: false,
-                },
-              },
-              fetchUsers: {
-                title: "Fetch Users",
-                key: "fetchUsers",
-                icon: "terminal",
-                schema: {
-                  label: "Data Source",
-                  persistent: "client-only",
-                  trigger: {
-                    init: true,
-                    server: false,
-                  },
-                  dataSrc: "url",
-                  fetch: {
-                    url: "https://fakestoreapi.com/users/1",
-                    method: "get",
-                    headers: [
-                      {
-                        key: "",
-                        value: "",
-                      },
-                    ],
-                    mapFunction:
-                      "// instance.root.getComponent('name').setValue(responseData.name);\n// instance.root.getComponent('age').setValue(responseData.age);\nconsole.log(responseData);\nvalue=responseData;",
-                    forwardHeaders: false,
-                    authenticate: false,
-                  },
-                  allowCaching: true,
-                  key: "dataSource",
-                  type: "datasource",
-                  input: true,
-                  tableView: false,
-                },
-              },
-              Password: {
-                title: "Password",
-                key: "Password",
-                schema: {
-                  label: "Password",
-                  applyMaskOn: "change",
-                  tableView: false,
-                  validate: {
-                    custom:
-                      "valid = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$!%*?&])[A-Za-z\\d@#$!%*?&]{8,}$/.test(input) ? true : 'Password must contain one uppercase, one lowercase, one digit, and one special character with a minimum length of 8 characters';",
-                  },
-                  key: "password",
-                  type: "password",
-                  input: true,
-                  protected: true,
-                  lockKey: true,
-                  source: "6575c84aef6488fe3abf4bbc",
-                  isNew: true,
-                  keyModified: true,
-                },
-              },
               Test: {
                 title: "Test",
                 key: "test",
@@ -380,7 +193,7 @@
               },
             },
           },
-          x: {
+          y: {
             title: "New category",
             weight: 10,
             components: {
